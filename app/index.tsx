67d4850c5e495372cb2d0dc16e74911d3ea98543
Evan Ordona <eordona@umd.edu>
2024-08-26 13:03:20 -0400

    init setup

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View className='items-center justify-center flex-1 border-2 border-red-400'>
+			<Text className="text-2xl">Workout Tracker</Text>
+		</View>
+	);
+}
